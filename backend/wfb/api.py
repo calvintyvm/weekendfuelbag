ff6e4f0ec5a633f8b41d29eb002f5b48aa450d12
Paul Nawat <scc385@gmail.com>
2018-09-22 16:50:52 -0700

    feat: enable filter by category and age

--- backend/wfb/api.py
+++ backend/wfb/api.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from .serializers import CategorySerializer, PlaceSerializer
 from .models import CategoryModel, PlaceModel
 from rest_framework import filters
+from django.db.models import Q
 
 
 class PlaceList(generics.ListCreateAPIView):
@@ -9,9 +10,32 @@ class PlaceList(generics.ListCreateAPIView):
     Access list of places
     """
     queryset = PlaceModel.objects.all()
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ('category__label', 'name')
     serializer_class = PlaceSerializer
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        queryset = PlaceModel.objects.all()
+
+        lat = self.request.query_params.get('lat', None)
+        lon = self.request.query_params.get('lon', None)
+        age = self.request.query_params.get('age', None)
+        category = self.request.query_params.get('category', None)
+
+        if lat is not None and lon is not None:
+            pass
+
+        if age is not None:
+            queryset = queryset.filter(
+                Q(min_age__lte=age) & Q(max_age__gte=age)
+            )
+
+        if category is not None:
+            queryset = queryset.filter(category__label__icontains=category)
+
+        return queryset
 
 
 class PlaceDetail(generics.RetrieveUpdateDestroyAPIView):
