8fd36c17ce69b0812e2468d86e5fd82264bdb702
Paul Nawat <scc385@gmail.com>
2018-09-22 13:32:06 -0700

    feat: add root endpoint

--- backend/wfb/views.py
+++ backend/wfb/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.permissions import AllowAny
+
+import os
+import json
+
 
 # Create your views here.
+
+
+@api_view(['GET'])
+@permission_classes((AllowAny,))  # todo: admin only in production
+def api_root(request, format=None):
+    return Response({
+        'locations': reverse('api_list_locations', request=request, format=format),
+        'categories': reverse('api_list_categories', request=request, format=format),
+    })
